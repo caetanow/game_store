a1e7891fbe4b3d5e78d7bd5060c8bc38b9900f2e
Willian de Jesus Caetano <willian.caetano@mercadolivre.com>
2022-11-29 15:11:46 -0300

    Projeto loja de games

--- loja_game/src/main/java/com/generation/loja_game/repository/ProdutosRepository.java
+++ loja_game/src/main/java/com/generation/loja_game/repository/ProdutosRepository.java
@@ -3,10 +3,12 @@ package com.generation.loja_game.repository;
 import com.generation.loja_game.model.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Repository
 public interface ProdutosRepository extends JpaRepository<Produtos, Long> {
 
     public List<Produtos> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);

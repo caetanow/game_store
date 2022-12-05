1b31363e46d5987458e297cf152c5e80567ac0ea
Willian de Jesus Caetano <willian.caetano@mercadolivre.com>
2022-12-05 15:47:24 -0300

    Implementado a Security

---
+++ loja_game/src/main/java/com/generation/loja_game/model/Usuario.java
@@ -0,0 +1,63 @@
+package com.generation.loja_game.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    @Email
+    private String usuario;
+
+    @NotNull
+    @Size(min = 8)
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
---
+++ loja_game/src/main/java/com/generation/loja_game/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.generation.loja_game.repository;
+
+import com.generation.loja_game.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Optional<Usuario> findByUsuario(String usuario);
+}
